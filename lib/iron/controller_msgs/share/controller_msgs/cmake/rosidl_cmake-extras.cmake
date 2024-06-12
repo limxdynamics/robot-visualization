0c7a7a39dc3393f043db2b31b39a0a2be19ded48
jenkins_ci <jenkins_ci@limxdynamics.com>
2024-06-12 18:52:32 +0800

    1.0.0.20240612182935

---
+++ lib/iron/controller_msgs/share/controller_msgs/cmake/rosidl_cmake-extras.cmake
@@ -0,0 +1,4 @@
+# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
+
+set(controller_msgs_IDL_FILES "msg/JointCmd.idl;msg/JointState.idl;msg/IMUData.idl")
+set(controller_msgs_INTERFACE_FILES "msg/JointCmd.msg;msg/JointState.msg;msg/IMUData.msg")
